2295e252f9452ffb9dbb87f7fac74920ab65ebc5
Gabe Glasco <gabeglasco@gmail.com>
2023-08-13 16:32:01 -0500

    working on clickItem function

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,25 +1,32 @@
+
+
 function GalleryItem(props) {
     // console.log('inside GalleryItem props is:', props)
     // console.log('props.item.path:', props.item.path)
+    
     const clickItem = () => {
-        console.log(props.item.id);
-        if(props.item.id) {
-            return (
+        console.log('props id is:', props.item.id);
+        console.log('props description:', props.item.description)
+            
+       if (props.item.id.clicked == true) {
+    
             <>
-            <p>{props.item.description}</p>
+            {props.item.description} 
             </>
-            )
+        
+       }
         }
-    }
+
     return (
         <>
         <img src ={props.item.path} 
             alt={props.item.description} 
             onClick={(clickItem)}/>
+        <button type="like">Love it!</button>
+        <p>{0} people love this</p>
         </>
     )
-
-
+    
 }
 
 export default GalleryItem
